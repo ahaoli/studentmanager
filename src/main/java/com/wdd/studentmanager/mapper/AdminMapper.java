d13f1443c6c0ada99115b1e60e053af4991620f3
test <abc@163.com>
2023-06-29 10:22:14 +0800

    删除了注释

--- src/main/java/com/wdd/studentmanager/mapper/AdminMapper.java
+++ src/main/java/com/wdd/studentmanager/mapper/AdminMapper.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AdminMapper {
 
-    Admin findByAdmin(Admin admin);
+    Admin findByAdmin(Admin admin);//验证 账号密码 正确性
 
 
-    int editPswdByAdmin(Admin admin);
+    int editPswdByAdmin(Admin admin);//修改管理员密码
 }
