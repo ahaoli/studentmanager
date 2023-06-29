b2d6b728c1fd7de7c8418b56f132b312318d0459
HY Z <924957250@qq.com>
2023-06-29 11:30:44 +0800

    2

--- src/main/java/com/wdd/studentmanager/domain/Clazz.java
+++ src/main/java/com/wdd/studentmanager/domain/Clazz.java
@@ -1,34 +1,18 @@
 package com.wdd.studentmanager.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * 班级实体类
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Clazz {
 	private Integer id;
 	private String name;
 	private String info;
 
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getInfo() {
-		return info;
-	}
-
-	public void setInfo(String info) {
-		this.info = info;
-	}
 }
